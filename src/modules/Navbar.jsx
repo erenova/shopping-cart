bfb3f6bbb1a3276ce15182707e6b2b13e1d7aea4
erenova <erenova6@gmail.com>
2024-11-29 09:26:11 +0300

    fix logo file location

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = ({ navs }) => {
   return (
     <div className="flex lg:gap-10 text-xl lg:py-6 lg:px-6">
       <Link to="/">
-        <img src="./cs2logo.png" className="w-20 lg:w-28 mt-2 ml-4" alt="" />
+        <img src="/../cs2logo.png" className="w-20 lg:w-28 mt-2 ml-4" alt="" />
       </Link>
       <div
         className="text-5xl fixed top-5 right-8 lg:hidden z-10"
